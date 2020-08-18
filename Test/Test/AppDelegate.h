7d9344552e15cfa98821b737a8f4f0a92fba3dd2
weiej <455026381@qq.com>
2020-08-18 16:57:52 +0800

    添加新工程

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by weienjie on 2020/8/18.
+//  Copyright © 2020 weiej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
